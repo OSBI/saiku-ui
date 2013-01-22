b10f114dfb80618508a3073bd8e6c821a94f3933
pstoellberger <pstoellberger@gmail.com>
2013-01-22 09:10:45 +0100

    add saikuolap query

---
+++ js/saiku/models/SaikuOlapQuery.js
@@ -0,0 +1,32 @@
+var SaikuOlapQuery = {
+  "queryModel": {
+    "axes": {
+      "FILTER": {
+        "location": "FILTER",
+        "hierarchies": {},
+        "nonEmpty": false,
+      },
+      "COLUMNS": {
+        "location": "COLUMNS",
+        "hierarchies": {},
+        "nonEmpty": false,
+      }, 
+      "ROWS": {
+        "location": "ROWS",
+        "hierarchies": {},
+        "nonEmpty": false,
+        "name": "ROWS"
+      }
+    },
+    "details": {
+      "axis": "COLUMNS",
+      "location": "BOTTOM",
+      "measures": []
+    },
+    "calculatedMeasures": []
+  },
+  "cube": {},
+  "mdx": null,
+  "name": "",
+  "type": "QUERYMODEL"
+}
